6d1398496a06ce9ab8e0da716d79b15ae5fa5284
Johan03Guerrero <40550092+Johan03Guerrero@users.noreply.github.com>
2023-05-02 11:34:07 -0500

    fix new map conceptos

--- app/models/concepto.rb
+++ app/models/concepto.rb
@@ -5,11 +5,11 @@ class Concepto < ApplicationRecord
 
 
   HASH_GASTOS_FUNCIONAMIENTO = {
-    "Pago Pensiones": ["3-34"],
-    "Nómina Administrativos": ["3-1-1-1", "3-1-1-01"],
-    "Nómina Docentes": ["3-1-2-1-01"],
-    "Nómina Trabajadores Oficiales": ["3-1-2-1-02-1", "3-1-2-1-02-10"],
-    "Profesores Hora Cátedra y Vinculación Especial": ["3-1-2-1-02-99-0", "3-1-2-1-02-99-01"],
-    "Gastos Generales": ["3-1-1-2"],
+    "Pago Pensiones": ["3-1-4-02", "3-01-004-02", "3-34"],
+    "Nómina Administrativos": ["3-01-001-01-01-01-0001-01", "3-1-1-1", "3-1-1-01"],
+    "Nómina Docentes": ["3-01-001-01-01-01-0001-02", "3-1-2-1-01"],
+    "Nómina Trabajadores Oficiales": ["3-01-001-03-01-01-0001", "3-1-1-03-01-01-0001", "3-1-2-1-02-1", "3-1-2-1-02-10"],
+    "Profesores Hora Cátedra y Vinculación Especial": ["3-01-001-02-01-01-0001", "3-1-2-1-02-99-0", "3-1-2-1-02-99-01"],
+    "Gastos Generales": ["3-01-002-02-02", "3-1-1-2"],
   }
 end
